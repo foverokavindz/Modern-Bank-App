c3cf61c708e08febfe9c1be8b117d429d487be49
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-02-06 23:55:57 +0530

    make navbar responsive

--- bank_modern_app/src/components/NavBar.jsx
+++ bank_modern_app/src/components/NavBar.jsx
@@ -23,9 +23,34 @@ const NavBar = () => {
         })}
       </ul>
 
-      <div className="sm.hidden flex flex-1 justify-end items-center">
-        <img src={toggle ? close : menu} alt="menu" />
-        classname = "w-28px" classmate
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          classname="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? 'flex ' : 'hidden'
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex  flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => {
+              return (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                    index === navLinks.length - 1 ? 'mr-0' : 'mb-4'
+                  } text-white mr-10`}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   );
