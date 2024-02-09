3e90f211fac61f1e96e40e43e04536ada7fe8d9c
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-02-09 22:49:26 +0530

    Done business section

--- bank_modern_app/src/components/Button.jsx
+++ bank_modern_app/src/components/Button.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
-
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ styles }) => {
+  return (
+    <button
+      className={`px-6 py-4 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 
 export default Button;
