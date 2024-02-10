961f90921d09025894d0f9d3d02720035054d68a
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-02-10 10:54:48 +0530

    prepare for deloy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Modern-Bank-App/bank_modern_app/',
+  base: '/bank_modern_app/',
   plugins: [react()],
 });
