2e139c4c028a64f8976fdab925e908f8115de041
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-02-10 11:16:35 +0530

    update base url

--- bank_modern_app/vite.config.js
+++ bank_modern_app/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Modern-Bank-App/bank_modern_app/',
   plugins: [react()],
 });
